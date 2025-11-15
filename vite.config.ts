64d603b51981e4dc109dc97c289262ce9b00be31
roiazari <roiazari148@gmail.com>
2025-11-15 16:52:34 +0200

    Update vite config with base path and rebuild for GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 👇 הוספת הנתיב הבסיסי של המאגר ב-GitHub Pages
+  base: '/my_profile/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
